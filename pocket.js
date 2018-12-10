8d2189ebaf0d382842a67750e57ecc1945a9ac84
Sal Denaro <sal@vc58.vc.panix.com>
2018-12-10 16:05:30 -0500

    added key handler

--- pocket.js
+++ pocket.js
@@ -1,9 +1,9 @@
-	function shareArticleToPocket(id, btn) {
+function shareArticleToPocket(id, btn) {
 	try {
 		var query = "?op=pluginhandler&plugin=oneclickpocket&method=getInfo&id=" + encodeURIComponent(id);
 
 		var d = new Date();
-	        var ts = d.getTime();
+		var ts = d.getTime();
 
 		Notify.progress("Saving to Pocket …", true);
 		new Ajax.Request("backend.php",	{
@@ -11,25 +11,29 @@
 			onSuccess: function(transport) {
 				var ti = JSON.parse(transport.responseText);
 				if (ti.status=="1") {
-				Notify.info("Saved to Pocket:<br/><em>" + ti.title + "</em>");
-				
-				btn.src='plugins/oneclickpocket/pocket.png';
-				btn.title='Saved to Pocket';
+					Notify.info("Saved to Pocket:<br/><em>" + ti.title + "</em>");
+
+					btn.src='plugins/oneclickpocket/pocket.png';
+					btn.title='Saved to Pocket';
 				} else {
-				Notify.error("<strong>Error saving to Pocket!</strong><br/>("+ti.status+")");}
+					Notify.error("<strong>Error saving to Pocket!</strong><br/>("+ti.status+")");}
 			} });
 
 	} catch (e) {
 		exception_error("PocketArticle", e);
 	}
-	}
-	
+};
 
-	hotkey_actions['pock_it'] = function() {
-		if (getActiveArticleId()) {
-		    var artid = "ocp"+getActiveArticleId();
-			shareArticleToPocket(getActiveArticleId(), document.getElementById(artid));
-			return;
-		}
-	};
-	
+
+require(['dojo/_base/kernel', 'dojo/ready'], function (dojo, ready) {
+	ready(function () {
+		PluginHost.register(PluginHost.HOOK_INIT_COMPLETE, () => {
+			App.hotkey_actions["pock_it"]  = function() {
+				if (Article.getActive()) {
+					postArticleToWallabag(Article.getActive());
+					return;
+				}
+			};
+		});
+	});
+});

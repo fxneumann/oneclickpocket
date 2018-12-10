c48661d2b0f1ec63fe3229869fc29b973ba36a2a
Sal Denaro <sal@vc58.vc.panix.com>
2018-12-10 15:57:07 -0500

    quick hacky fix

--- pocket.js
+++ pocket.js
@@ -1,30 +1,22 @@
-	hotkey_actions['pock_it'] = function() {
-		if (getActiveArticleId()) {
-		    var artid = "ocp"+getActiveArticleId();
-			shareArticleToPocket(getActiveArticleId(), document.getElementById(artid));
-			return;
-		}
-	};
-	
 	function shareArticleToPocket(id, btn) {
 	try {
-		var query = "?op=pluginhandler&plugin=oneclickpocket&method=getInfo&id=" + param_escape(id);
+		var query = "?op=pluginhandler&plugin=oneclickpocket&method=getInfo&id=" + encodeURIComponent(id);
 
 		var d = new Date();
 	        var ts = d.getTime();
 
-		notify_progress("Saving to Pocket …", true);
+		Notify.progress("Saving to Pocket …", true);
 		new Ajax.Request("backend.php",	{
 			parameters: query,
 			onSuccess: function(transport) {
 				var ti = JSON.parse(transport.responseText);
 				if (ti.status=="1") {
-				notify_info("Saved to Pocket:<br/><em>" + ti.title + "</em>");
+				Notify.info("Saved to Pocket:<br/><em>" + ti.title + "</em>");
 				
 				btn.src='plugins/oneclickpocket/pocket.png';
 				btn.title='Saved to Pocket';
 				} else {
-				notify_error("<strong>Error saving to Pocket!</strong><br/>("+ti.status+")");}
+				Notify.error("<strong>Error saving to Pocket!</strong><br/>("+ti.status+")");}
 			} });
 
 	} catch (e) {
@@ -33,3 +25,11 @@
 	}
 	
 
+	hotkey_actions['pock_it'] = function() {
+		if (getActiveArticleId()) {
+		    var artid = "ocp"+getActiveArticleId();
+			shareArticleToPocket(getActiveArticleId(), document.getElementById(artid));
+			return;
+		}
+	};
+	
